f48cba4ec60e722d8405017d1de5c05d63101c18
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-22 21:30:44 +0530

    printing in single line concat

---
+++ Mady.java
@@ -0,0 +1,7 @@
+class Mady{
+	public static void main(String[] args){
+	String []winter={"january","february","march","april","may","june","july","august","september","october","november","december"};
+	 System.out.println(winter[0]+""+winter[1]+""+winter[2]+""+winter[3]+""+winter[4]+""+winter[5]+""+winter[6]+""+winter[7]+""+winter[8]+""+winter[9]+""+winter[10]+""+winter[11]);
+	  }
+	  }
+	  
