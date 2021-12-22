ae1793885086687088c2617eede3dddc97ee0edc
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-22 21:28:39 +0530

    printing in single line concat

---
+++ Handling.java
@@ -0,0 +1,7 @@
+class Handling{
+	public static void main(String []uu){
+	 String[] days={"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
+		System.out.println(days[0]+""+days[1]+""+days[2]+""+days[3]+""+days[4]+""+days[5]+""+days[6]);
+		 }
+		 }
+		 
