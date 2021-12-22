8b250b29be532eddc072c9632de4371ecd4c3c9d
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-22 21:23:21 +0530

    using for loop

---
+++ Cellphone.java
@@ -0,0 +1,10 @@
+class Cellphone{
+  public static void main(String []umm){
+         String []numbers={"123456789","7026637798","9743675131","6363977693","123456790","123456987","1234567890"};
+		 for(int index=0; index<numbers.length;index=index+1){
+		 
+		 System.out.println(numbers[index]);
+		 }
+		 }
+		 }
+		 
