966a98816343fb97a4502c5844c55fec624f17e8
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-22 21:22:15 +0530

    using for loop

---
+++ Months.java
@@ -0,0 +1,9 @@
+class Months{
+	public static void main(String[] args){
+	String []winter={"january","february","march","april","may","june","july","august","september","october","november","december"};
+	  for(int index=0;index<winter.length;index=index+1){
+	  System.out.println(winter[index]);
+	  }
+	  }
+	  }
+	  
