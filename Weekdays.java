97d4804a77ba4ab80493fa04610672aa88858d42
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-22 21:16:47 +0530

    using for loop

---
+++ Weekdays.java
@@ -0,0 +1,9 @@
+class Weekdays{
+	public static void main(String []uu){
+	 String[] days={"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
+		for(int index=0; index<days.length;index=index+1){
+		 System.out.println(days[index]);
+		 }
+		 }
+		 }
+		 
