b12da9580552ba6815a6f16c83803e313147e644
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-22 21:27:08 +0530

    ArrayIndexOutOfBoundsException

---
+++ Festivals.java
@@ -0,0 +1,9 @@
+class Festivals{
+	public static void main(String []uu){
+	 String[] india={"diwali","holi","dussehra","ganesh chaturthi","navaratri","raksha bandhan","ayudha puja"};
+		for(int index=0; index<=india.length;index=index+1){
+		 System.out.println(india[index]);
+		 }
+		 }
+		 }
+		 
